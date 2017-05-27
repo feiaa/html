afff5af54f8c0ac8a305eea655f1d039011e2e74
yyf <yyf@yyf>
2017-05-27 08:52:48 +0800

    Sat May 27 08:52:48 CST 2017

--- template.php
+++ template.php
@@ -214,3 +214,20 @@
     </div>
     </div>
 </div>
+
+
+
+
+            <h2 onclick="atest('myModal')">MNIST</h2>
+            <div class="contest">
+                <div class="contest-abstract">
+                <p>
+                You have some experience with R or Python and machine learning basics, but you’re new to computer vision. This competition is the perfect introduction to techniques like neural networks using a classic dataset including pre-extracted features.</p><p>
+                MNIST ("Modified National Institute of Standards and Technology") is the de facto “hello world” dataset of computer vision. Since its release in 1999, this classic dataset of handwritten images has served as the basis for benchmarking classification algorithms. As new machine learning techniques emerge, MNIST remains a reliable resource for researchers and learners alike.</p><p>
+                In this competition, your goal is to correctly identify digits from a dataset of tens of thousands of handwritten images. We’ve curated a set of tutorial-style kernels which cover everything from regression to neural networks. We encourage you to experiment with different algorithms to learn first-hand what works well and how techniques compare.</p>
+
+                </div>
+            </div>
+            <div class="clearFloat"></div>
+            <a href="#." onclick="atest('myModal')" class="contest-more">详情>></a>
+            <div class="clearFloat"></div>
